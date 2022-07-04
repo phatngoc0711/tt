59d50f1223010d106c84e00ca07ee8a5bf80762f
phatngoc0711 <phat07112001@gmail.com>
2022-07-05 00:27:49 +0700

    1

---
+++ SearchAppTest/SearchAppTest/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim table As New DataTable()
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        table.Columns.Add("ID", Type.GetType("System.Int32"))
+        table.Columns.Add("FirstName", Type.GetType("System.Int32"))
+        table.Columns.Add("LastName", Type.GetType("System.Int32"))
+        table.Columns.Add("Age", Type.GetType("System.Int32"))
+        DataGridView1.DataSource = table
+    End Sub
+End Class
