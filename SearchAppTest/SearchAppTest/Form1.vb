80fd7786b4179e709e3fad24f22dc8fafe8a9337
phatngoc0711 <phat07112001@gmail.com>
2022-07-05 23:09:33 +0700

    3

--- SearchAppTest/SearchAppTest/Form1.vb
+++ SearchAppTest/SearchAppTest/Form1.vb
@@ -7,6 +7,7 @@ Public Class Form1
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnMode.Fill
         table.Columns.Add("ID", Type.GetType("System.String"))
         table.Columns.Add("FirstName", Type.GetType("System.String"))
         table.Columns.Add("LastName", Type.GetType("System.String"))
@@ -19,14 +20,20 @@ Public Class Form1
         line = File.ReadAllLines("C:\Users\phat0\Desktop\test.idx")
 
         For i As Integer = 0 To line.Length - 1 Step +1
-            vals = line(i).ToString().Split(" ")
-            Dim row(vals.Length - 1) As String
+            vals = line(i).ToString().Split("	")
+            Dim row(vals.Length - 2) As String
 
-            For j As Integer = 0 To vals.Length - 1 Step +1
+            For j As Integer = 0 To vals.Length - 2 Step +1
                 row(j) = vals(j).Trim()
             Next j
             table.Rows.Add(row)
         Next i
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim form As New Form2
+        form.Show()
 
     End Sub
 End Class
---
+++ SearchAppTest/SearchAppTest/Form2.vb
@@ -0,0 +1,45 @@
+﻿Imports System.IO
+Imports System.Text
+Imports Microsoft.VisualBasic
+
+Public Class Form2
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'get name workType
+        Dim line() As String
+        line = File.ReadAllLines("C:\Users\phat0\Desktop\test.txt")
+
+        For i As Integer = 0 To line.Length - 1 Step +1
+            Dim sb As New StringBuilder()
+            For Each c As Char In line(i)
+                If [Char].IsLetter(c) Then
+                    sb.Append(c)
+                End If
+            Next
+            ListBox1.Items.Add(sb)
+        Next i
+        '---------------------------------------------------------------------
+
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        TextBox1.MaxLength = 5
+        TextBox2.TextAlign = HorizontalAlignment.Right
+        TextBox2.MaxLength = 10
+
+        '---------------------------------------------------------------------
+
+    End Sub
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not e.KeyChar = Chr(Keys.Delete) And Not e.KeyChar = Chr(Keys.Back) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub TextBox2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox2.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not e.KeyChar = Chr(Keys.Delete) And Not e.KeyChar = Chr(Keys.Back) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    End Sub
+End Class
